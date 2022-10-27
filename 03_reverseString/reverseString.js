6283ba18663667109533dfafd7530dec28e3d777
EmpathMom <gaylenedavison87@gmail.com>
2022-10-27 13:15:04 -0700

    finished sumAll

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+const arrayStrings = str.split("");
+const reverseArray = arrayStrings.reverse();
+const joinArray = reverseArray.join("");
+return joinArray;
+}
+const String =("hello there");
+const result = reverseString(string);
+console.log(result);
 
 // Do not edit below this line
 module.exports = reverseString;
